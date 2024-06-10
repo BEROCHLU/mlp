5ff885a6b35678ef9d73bf6faa323cb5f8b7bcb5
BEROCHLU <42054433+BEROCHLU@users.noreply.github.com>
2024-06-10 10:32:10 +0900

    Delete sakata.sh

--- sakata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm ./result/output.log
-# batchフォルダ内の全ての.jsonファイルに対してループ
-for file in ./batch/*.json; do
-    # ファイルの内容をapp.pyに渡す
-    cat "$file" | python inputstd.py >> ./result/output.log
-done
